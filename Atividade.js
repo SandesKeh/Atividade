89e60be77775c1e56aa09dedbe5214e93f5ceedd
Kauaa123 <ra49014845839@acaonsfatima.org.br>
2024-08-02 09:45:08 -0300

    subindo

--- Atividade.js
+++ Atividade.js
@@ -1,7 +1,7 @@
 import express from 'express'
-
-
 const servidor= express ()
+
+servidor.use(express.json())
 
 servidor.get ('/calcular/somatoria', (req,resp) => {
 
@@ -12,15 +12,38 @@ let somar = soma + soma1
 
 resp.send ({
     somar : somar 
+})
+} )
 
+servidor.get('/calculadora/sub', (req, resp) => {
+    let n1 = Number(req.query.n1)
+    let n2 = Number(req.query.n2)
 
+    let diminuir = n1 - n2
 
+    resp.send({
+        entrada:{
+            números: n1,
+            numeros: n2
+        },
+        diminuir: diminuir
+    })
+})
+
+servidor.post('/calculadora', (req, resp) => {
+    let n1 = req.body.numero1
+    let n2 = req.body.numero2
+
+    let soma = n1 + n2
+
+    resp.send({
+        entrada:{
+            números: n1,
+            numeros: n2
+        },
+        soma: soma
+    })
 })
 
 
-} )
-
-
-
-
-servidor.listen (3001, ()=> console.log("API subiu ") )
+servidor.listen (3001, ()=> console.log("A API subiu na porta 3001!") )
